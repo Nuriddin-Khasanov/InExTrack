bfabd85d3be3b294f8a3076557f8821b7fce8764
Nuriddin-Khasanov <nuriddinhasanov44@gmail.com>
2025-09-06 12:20:59 +0500

    Delete_User

--- InExTrack/Interfaces/Services/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InExTrack.Interfaces.Services
-{
-    public interface IAuthService
-    {
-        Task<string?> AuthenticateAsync(string username, string password);
-        Task<bool> RegisterUserAsync(string username, string password);
-    }
-}
